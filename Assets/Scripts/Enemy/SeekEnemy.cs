0cb181d3e595fe324f180af7ec9ae2bc0e39391d
mehmeteyupkatirci <mehmeteyupkatirci@gmail.com>
2024-06-23 14:46:17 +0300

    new enemy added (seekEnemy.cs added)

---
+++ Assets/Scripts/Enemy/SeekEnemy.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class SeekEnemy : MonoBehaviour
+{
+    public float speed = 3f; // Düşmanın hareket hızı
+    private Transform player;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    private void Update()
+    {
+        if (player != null)
+        {
+            Vector2 direction = (player.position - transform.position).normalized;
+            transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            PlayerHealth playerHealth = collision.gameObject.GetComponent<PlayerHealth>();
+            if (playerHealth != null)
+            {
+                playerHealth.TakeDamage(1); // Hasar miktarını buradan ayarlayabilirsiniz
+            }
+        }
+    }
+}
